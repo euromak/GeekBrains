af881a442b00ab81ec25056214a8d70a84a2a96d
euromak <euromak2016@yandex.ru>
2019-08-21 01:06:01 +0300

    Added vue component of render catalog products on main page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 'use strict';
 
+Vue.component('catalog-item', {
+    props: ['data'],
+
+    template: `
+        <div class="products-grid-items">
+            <a href="page.html">
+                <div class="img-wrap">
+                    <img :src="data.image" :alt="data.name" class="product-image">
+                 </div>
+                 <div class="product-name">{{data.name}}</div>
+                 <div class="price">$ {{data.price}}</div>
+            </a>
+            <a href="" class="product-hover-block">
+            <img src="img/cart-white.svg" alt="cart" class="product-icon-cart">Add to Cart</a>
+        </div>    
+    `,
+});
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -14,6 +32,12 @@ const app = new Vue({
     },
 
     mounted() {
-        this.getJSON(this.productsDataURL).then(data => console.log(data)).catch(error => console.log(error));
+        return this.getJSON(this.productsDataURL)
+            .then(data => {
+                data.forEach((item) => {
+                  this.productsData.push(item);
+                })
+            }).catch(error => console.log(error));
     }
 });
+
