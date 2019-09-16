fa3681cfc6035f374387268c25a90d3fe818e82c
euromak <euromak2016@yandex.ru>
2019-09-16 23:49:41 +0300

    Render products data to catalog grid on main page

--- public/js/ProductsComp.js
+++ public/js/ProductsComp.js
@@ -13,7 +13,10 @@ Vue.component('catalog', {
     },
 
     methods: {
-
+        filter(value){
+            let regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
     },
     mounted(){
         this.$parent.getJson('/api/products')
@@ -25,18 +28,22 @@ Vue.component('catalog', {
             });
     },
 
+    template: `<div class="products-grid"><product v-for="item of filtred" :key="item.id_product" :img="imgCatalog" :product="item"></product></div>`,
+});
+
+Vue.component('product', {
+   props: ['product', 'img'],
     template: `
         <div class="products-grid-items">
             <a href="page.html">
                 <div class="img-wrap">
-                    <img class="product-image">
+                    <img :src="product.image" class="product-image">
                  </div>
-                 <div class="product-name"></div>
-                 <div class="price">$ </div>
+                 <div class="product-name">{{product.product_name}}</div>
+                 <div class="price">$ {{product.price}}</div>
             </a>
             <a href="" class="product-hover-block">
             <img src="img/cart-white.svg" alt="cart" class="product-icon-cart">Add to Cart</a>
         </div>    
     `,
 });
-
